5f95c88239440adeb7785c647203dfc282849f5c
tobit bushenyula <mrbushenyula@gmail.com>
2024-01-15 09:24:16 +0300

    Create SQL_bootcamp_challenges.sql
    this is the first SQL challenge. we are asked to select all the record in the columns first-name and last-name.

---
+++ SQL_bootcamp_challenges.sql
@@ -0,0 +1,6 @@
+-- CHALLENGE ONE
+
+SELECT first_name,last_name
+FROM hcm.employees;  -- this code here will select all the records in 
+                     -- the column first_name and last_name
+
