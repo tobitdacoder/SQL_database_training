510900be4aa3016be2d90817506370740ef5dd41
tobit bushenyula <mrbushenyula@gmail.com>
2024-01-16 10:40:04 +0300

    Update SQL_bootcamp_challenges.sql
    here e are using the AS keyword to return a renamed field

--- SQL_bootcamp_challenges.sql
+++ SQL_bootcamp_challenges.sql
@@ -6,3 +6,10 @@ FROM hcm.employees;  -- this code here will select all the records in
 
 SELECT first_name,last_name, coalesce(state_province,'N/A') as 	State_province
 FROM hcm.employees;
+
+-- CHALLENGE TWO
+
+SELECT last_name AS customer_last_name,city
+FROM hcm.employees;
+
+-- here e are using the AS keyword to return a renamed field.
