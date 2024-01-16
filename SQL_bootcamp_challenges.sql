f042abe37963b830445d047f99c06236eef5970d
tobit bushenyula <mrbushenyula@gmail.com>
2024-01-16 10:38:43 +0300

    Update SQL_bootcamp_challenges.sql
    here we are writing a query that will return some columns but here we are making sure that we don't return NULL values by using the COALESCE function which helps replace NULL values.

--- SQL_bootcamp_challenges.sql
+++ SQL_bootcamp_challenges.sql
@@ -4,3 +4,5 @@ SELECT first_name,last_name
 FROM hcm.employees;  -- this code here will select all the records in 
                      -- the column first_name and last_name
 
+SELECT first_name,last_name, coalesce(state_province,'N/A') as 	State_province
+FROM hcm.employees;
