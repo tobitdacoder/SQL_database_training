b53075388efafe3179bf4ec004c25faaf9938cc6
tobit bushenyula <mrbushenyula@gmail.com>
2024-01-26 11:50:49 +0300

    Update SQL_bootcamp_challenges.sql
    this is the right outer join.

--- SQL_bootcamp_challenges.sql
+++ SQL_bootcamp_challenges.sql
@@ -96,6 +96,15 @@ ON d.dept_id = e.dept_id;
 SELECT d.dept_id, d.dept_name, e.emp_id, e.first_name
 FROM departments d LEFT JOIN employees e
 ON d.dept_id = e.dept_id;
+
+
+-- RIGHT OUTER JOIN:
+
+SELECT d.dept_id, d.dept_name, e.emp_id, e.first_name
+FROM employees e RIGHT OUTER JOIN departments d
+ON d.dept_id = e.dept_id;
+
+
 -- --------------------------------------------------------------
 
 
