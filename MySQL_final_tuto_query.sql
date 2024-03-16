79e9e8f5776468b67b85efa52dba61eb103007d0
tobit bushenyula <mrbushenyula@gmail.com>
2024-03-16 10:57:09 +0300

    Update MySQL_final_tuto_query.sql
    the first table has been created.

--- MySQL_final_tuto_query.sql
+++ MySQL_final_tuto_query.sql
@@ -1,5 +1,13 @@
 create database final;
 
--- use final;
+-- use final;  this code is used to help us not always specify the db in the code we are writing
+-- since we are already usingthe database (we are in the database)
 
 select database();
+
+create table pastries (
+name varchar(50),
+quantity int
+);
+
+desc pastries;
